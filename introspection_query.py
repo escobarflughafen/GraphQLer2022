b0868e523eff7c4098dcc7a0b0e11b7df45aed9f
Neo Cheung <angel8878@gmail.com>
2022-10-19 00:10:01 -0700

    separate introspection query

---
+++ introspection_query.py
@@ -0,0 +1,112 @@
+import requests, functools, json
+
+def send_request(url):
+
+	body = {
+		"query": """query IntrospectionQuery {
+			__schema {
+				queryType { name }
+				mutationType { name }
+				subscriptionType { name }
+				types {
+					...FullType
+				}
+				directives {
+					name
+					description
+
+					locations
+					args {
+						...InputValue
+					}
+				}
+	  		}
+		}
+
+		fragment FullType on __Type {
+			kind
+			name
+			description
+
+			fields(includeDeprecated: true) {
+				name
+				description
+				args {
+				...InputValue
+				}
+				type {
+				...TypeRef
+				}
+				isDeprecated
+				deprecationReason
+			}
+
+			inputFields {
+				...InputValue
+			}
+
+			interfaces {
+				...TypeRef
+			}
+
+			enumValues(includeDeprecated: true) {
+				name
+				description
+				isDeprecated
+				deprecationReason
+			}
+
+			possibleTypes {
+				...TypeRef
+			}
+		}
+
+		fragment InputValue on __InputValue {
+			name
+			description
+			type { 
+				...TypeRef 
+			}
+			defaultValue
+		}
+
+		fragment TypeRef on __Type {
+			kind
+			name
+			ofType {
+				kind
+				name
+				ofType {
+					kind
+					name
+					ofType {
+						kind
+						name
+						ofType {
+							kind
+							name
+							ofType {
+								kind
+								name
+								ofType {
+									kind
+									name
+									ofType {
+										kind
+										name
+									}
+								}
+			  				}
+						}
+		  			}
+				}
+	  		}
+		}
+		"""
+	}
+
+	x = requests.post(
+		url=url,
+		json=body
+	)
+	return json.loads(x.text)
