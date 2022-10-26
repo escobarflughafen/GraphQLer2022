e9b0db3e495ef45ffc0f772d0a4015df84f78f73
NeoTheOne88 <angel8878@gmail.com>
2022-10-25 20:15:16 -0700

    add some trash

--- dependency_check_workflow.py
+++ dependency_check_workflow.py
@@ -1,17 +1,72 @@
 from ast import Delete
+from distutils.command.build import build
+from random import random
+from readline import append_history_file
 from turtle import update
 import requests, functools, json
+
+from get_introspection_type import parse_data_type
 
 
 datadb = {}
 fuzz_int = [0,1,-1]
 fuzz_string = ["wfr23","@$@UD@", " "]
+fuzz_float = [12.123, 141.123124, .124]
 fuzz_bool = ['false', 'true']
 fuzz_id = ["14314", "qrqf2132", "@(Q@QQD"]
 
+data_type_json = parse_data_type(connect.get_introspection(url="http://neogeek.io:4000/graphql"))
 
-def build_query_string_input(config):
-    config["consumes"]
+test_json = {"name": "getMessage", "produces": {"kind": "OBJECT", "name": "Message"}, "consumes": [{"name": "id", "type": {"kind": "NON_NULL", "name": null, "ofType": {"kind": "SCALAR", "name": "ID"}}}]}
+test_object_list = {}
+
+def fuzz_scalar(kind):
+    if kind == "String":
+        return random.choice(fuzz_string)
+    elif kind == "Int":
+        return random.choice(fuzz_int)
+    elif kind == "Float":
+        return random.choice(fuzz_float)
+    elif kind == "Boolean":
+        return random.choice(fuzz_bool)
+    elif kind == "ID":
+        return random.choice(fuzz_id)
+
+def get_type(type):
+    if type["name"] == None:
+        return get_type(type["ofType"])
+    else:
+        return type
+
+
+def build_query_string(json_string):
+    inner_json = {}
+    query = "{" + json_string["name"] + "("
+    for arg in json_string["consumes"]:
+        data_type = get_type(arg)
+        if data_type["kind"] == "SCALAR":
+            inner_json[arg["name"]] = fuzz_scalar(data_type["name"])
+        else:
+            inner_json[arg["name"]] = build_object(search_object(data_type_json, data_type["name"]))
+    query += json.dumps(inner_json) + ")\{\}\}"
+    return query
+
+
+def search_object(object_json, object_name):
+    return object_json[object_name]
+
+
+def build_object(object):
+    inner_json = {}
+    for arg in object["params"]:
+        data_type = get_type(arg)
+        if data_type["kind"] == "SCALAR":
+            inner_json[arg["name"]] = fuzz_scalar(data_type["name"])
+        else:
+            inner_json[arg["name"]] = build_object(search_object(data_type_json, data_type["name"]))
+    return inner_json
+
+
 
 
 def send_request(url, query):
@@ -52,7 +107,3 @@ def dependency_check():
             delete datadb[datatype]
 
 
-def build_query_string_input(query_json):
-    query_json["name"]
-    query_json["consumes"]
-    query_json["produces"]
