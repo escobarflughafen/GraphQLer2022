09281e0e443fec06dfcf5b8b3b79098961679296
NeoTheOne88 <angel8878@gmail.com>
2022-12-09 15:49:30 -0800

    add server setup

---
+++ test_server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+import {ApolloServer} from 'apollo-server-express';
+import {createServer} from 'http';
+import cors from 'cors';
+import schema from './data/schema';
+
+const PORT = 4001;
+const app = express();
+
+app.use('*', cors());
+
+const server = new ApolloServer({
+    schema,
+    playground: true,
+})
+
+server.applyMiddleware({app});
+
+const httpServer = createServer(app);
+
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(`馃殌 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  console.log(`馃殌 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`);
+});
